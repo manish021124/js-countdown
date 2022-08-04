d9d59d05b4053c16076513459a42d387e3e6ee52
manish021124 <maneeshbalami@gmail.com>
2022-08-04 13:58:18 +0545

    update

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const currentDate = new Date();
+const currentDate = Date.now();
 let data = document.getElementById('data');
 
 document.getElementById('button').addEventListener("click", getData);
@@ -6,6 +6,13 @@ console.log(currentDate);
 function getData(){
     let date = data.elements[0].value;
     let time = data.elements[1].value;
+
+    let a = Date.parse(date);
+
+    let b = a - currentDate;
+    let c = b / (24*60*60*1000);
+    console.log(a);
+    console.log(c);
 }
 
 
