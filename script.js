a93974d6df9c66e600cacb0a79ae67e88a3a5fd8
manish021124 <maneeshbalami@gmail.com>
2022-08-04 18:45:26 +0545

    update

--- script.js
+++ script.js
@@ -4,15 +4,23 @@ let data = document.getElementById('data');
 document.getElementById('button').addEventListener("click", getData);
 console.log(currentDate);
 function getData(){
-    let date = data.elements[0].value;
-    let time = data.elements[1].value;
+    let inputDate = data.elements[0].value;
+    let inputTime = data.elements[1].value;
+    let totalInput = inputDate + " " + inputTime;
+    let inputDateTime = new Date(totalInput);
+    let inputMilli = inputDateTime.getTime();
+    console.log(inputMilli);
 
-    let a = Date.parse(date);
+    let countingTime = inputMilli - currentDate;
+    let countingDays = Math.floor(countingTime / (24 * 60 * 60 * 1000));
+    let countingHours = Math.floor(countingTime / (60 * 60 * 1000));
+    console.log(countingDays);
+    console.log(countingHours);
 
-    let b = a - currentDate;
-    let c = b / (24*60*60*1000);
-    console.log(a);
-    console.log(c);
+    // let d = time * 1000 * 60 * 60;
+
+    // console.log(time);
+    // console.log(d);
 }
 
 
