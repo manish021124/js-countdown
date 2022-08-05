e7dbf198ea39836f8b44158b554fc7069be35683
manish021124 <maneeshbalami@gmail.com>
2022-08-05 14:00:01 +0545

    update

--- script.js
+++ script.js
@@ -1,33 +1,36 @@
 const currentDate = Date.now();
-let data = document.getElementById('data');
+const data = document.getElementById('data');
 
 document.getElementById('button').addEventListener("click", start);
 console.log(currentDate);
 
 function start(){
     getData();
-    dataToMilli();
-    calculateCount();
 }
 
 function getData(){
     let inputDate = data.elements[0].value;
     let inputTime = data.elements[1].value;
+
+    dataToMilli(inputDate, inputTime);
 }
 
-function dataToMilli(){
-    let totalInput = inputDate + " " + inputTime;
+function dataToMilli(date, time){
+    let totalInput = date + " " + time;
     let inputDateTime = new Date(totalInput);
     let inputMilli = inputDateTime.getTime();
+
     console.log(inputMilli);
+    calculateCount(inputMilli);
 }
 
-function calculateCount(){
-    let countingTime = inputMilli - currentDate;
+function calculateCount(milli){
+    let countingTime = milli - currentDate;
     let countingDays = Math.floor(countingTime / (24 * 60 * 60 * 1000));
     let countingHours = Math.floor((countingTime % (24 * 60 * 60 * 1000)) / (60 * 60 * 1000));
     let countingMinutes = Math.floor((countingTime % (60 * 60 * 1000)) / (60 * 1000));
     let countingSeconds = Math.floor((countingTime % (60 * 1000)) / (1000));
+
     console.log(countingTime);
     console.log(countingDays);
     console.log(countingHours);
