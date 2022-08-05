5bfc148444d24d2a580939a82f9408fb871d5425
manish021124 <maneeshbalami@gmail.com>
2022-08-05 14:28:01 +0545

    update

--- script.js
+++ script.js
@@ -4,36 +4,41 @@ const data = document.getElementById('data');
 document.getElementById('button').addEventListener("click", start);
 
 function start(){
-    getData();
-}
-
-function getData(){
     let inputDate = data.elements[0].value;
     let inputTime = data.elements[1].value;
-
-    dataToMilli(inputDate, inputTime);
-}
-
-function dataToMilli(date, time){
-    let totalInput = date + " " + time;
+    let totalInput = inputDate + " " + inputTime;
     let inputDateTime = new Date(totalInput);
     let inputMilli = inputDateTime.getTime();
-
-    calculateCount(inputMilli);
-}
-
-function calculateCount(milli){
-    let countingTime = milli - currentDate;
+    let countingTime = inputMilli - currentDate;
     let countingDays = Math.floor(countingTime / (24 * 60 * 60 * 1000));
     let countingHours = Math.floor((countingTime % (24 * 60 * 60 * 1000)) / (60 * 60 * 1000));
     let countingMinutes = Math.floor((countingTime % (60 * 60 * 1000)) / (60 * 1000));
     let countingSeconds = Math.floor((countingTime % (60 * 1000)) / (1000));
+    const dayCount = document.getElementById('daycount');
+    const hourCount = document.getElementById('hourcount');
+    const minCount = document.getElementById('mincount');
+    const secCount = document.getElementById('seccount');
+
 
     console.log(countingDays);
     console.log(countingHours);
     console.log(countingMinutes);
     console.log(countingSeconds);
 
+    displayCount(dayCount, countingDays);
+    displayCount(hourCount, countingHours);
+    displayCount(minCount, countingMinutes);
+    displayCount(secCount, countingSeconds);
+
+    // setTimeout(start, 1000);
+}
+
+function displayCount(count, counting){
+    if(counting < 10){
+        count.innerHTML = "0" + counting;
+    }else{
+        count.innerHTML = counting;
+    }
 }
 
 
